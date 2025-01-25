a22d6fabeb78ad747af51722a62214e83ebfabdb
saurabh <kutwalsaurabh19@gmail.com>
2025-01-25 23:02:57 +0530

    testngxml aded.

--- src/test/java/com/jenkins/DemoTest.java
+++ src/test/java/com/jenkins/DemoTest.java
@@ -1,4 +1,28 @@
 package com.jenkins;
 
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
 public class DemoTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Inside Test 1");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Inside Test 2");
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Inside Test 3");
+        throw new SkipException("Skipping test");
+    }
+
+
 }
